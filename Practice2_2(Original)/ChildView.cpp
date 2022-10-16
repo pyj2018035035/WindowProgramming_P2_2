c9a87e162af75b33c75320707984e48744f6d088
pyj2018035035 <115928706+pyj2018035035@users.noreply.github.com>
2022-10-16 21:56:06 +0900

    Add files via upload

---
+++ Practice2_2(Original)/ChildView.cpp
@@ -0,0 +1,131 @@
+﻿
+// ChildView.cpp: CChildView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Practice2_2.h"
+#include "ChildView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CChildView
+
+CChildView::CChildView()
+{
+}
+
+CChildView::~CChildView()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+	ON_WM_SIZE()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_RBUTTONDOWN()
+	ON_WM_KEYDOWN()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+
+
+// CChildView 메시지 처리기
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) 
+{
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(nullptr, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), nullptr);
+
+	return TRUE;
+}
+
+void CChildView::OnPaint() 
+{
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+	
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	dc.TextOut(10, 10, m_strWindowSize);
+	CRect rect;
+	GetClientRect(&rect);
+	dc.DrawText(m_strOutput, rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+	
+	// 그리기 메시지에 대해서는 CWnd::OnPaint()를 호출하지 마십시오.
+}
+
+
+
+void CChildView::OnSize(UINT nType, int cx, int cy)
+{
+	CWnd::OnSize(nType, cx, cy);
+
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	m_strWindowSize.Format(_T("윈도우 크기는 넓이 %d, 높이 %d입니다."), cx, cy);
+
+	Invalidate();
+}
+
+
+void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_bDrag = TRUE;
+	m_strOutput = _T("왼쪽 마우스 버튼을 눌렀습니다.");
+	Invalidate();
+
+	CWnd::OnLButtonDown(nFlags, point);
+}
+
+
+void CChildView::OnRButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_strOutput = _T("오른쪽 마우스 버튼을 눌렀습니다.");
+	Invalidate();
+
+	CWnd::OnRButtonDown(nFlags, point);
+}
+
+
+void CChildView::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_strOutput = _T("키보드를 눌렀습니다.");
+	Invalidate();
+
+	CWnd::OnKeyDown(nChar, nRepCnt, nFlags);
+}
+
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	if (m_bDrag == TRUE) {
+		m_strOutput = _T("마우스를 드래그하고 있습니다.");
+	}
+	else {
+		m_strOutput = _T("마우스를 이동 중입니다.");
+	}
+	Invalidate();
+
+	CWnd::OnMouseMove(nFlags, point);
+}
+
+
+void CChildView::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_bDrag = FALSE;
+
+	CWnd::OnLButtonUp(nFlags, point);
+}
---
+++ Practice2_2(Original)/Practice2_2.h
@@ -0,0 +1,41 @@
+﻿
+// Practice2_2.h: Practice2_2 응용 프로그램의 기본 헤더 파일
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 주 기호입니다.
+
+
+// CPractice22App:
+// 이 클래스의 구현에 대해서는 Practice2_2.cpp을(를) 참조하세요.
+//
+
+class CPractice22App : public CWinAppEx
+{
+public:
+	CPractice22App() noexcept;
+
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 구현입니다.
+
+public:
+	BOOL  m_bHiColorIcons;
+
+	virtual void PreLoadState();
+	virtual void LoadCustomState();
+	virtual void SaveCustomState();
+
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPractice22App theApp;
